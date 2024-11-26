83036deb9f1ee204521a9fff98d059233273929d
Harold6789 <648772@eastridingcollege.ac.uk>
2024-11-26 11:30:42 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<div class="header">
+</div>
+<div class="header-right">
+    <a class="buttonClass active" href="new_post_form.php">Create Posts</a>
+    <a class="buttonClass active" href=>Search Posts</a>
+    <a class="buttonClass active" href=>View Profile</a>
+    <a class="buttonClass active" href=>Create Bio</a>
+    <a class="buttonClass active" href=>Feedback</a>
+  </div>
+
+<div class="Homepagetxt">
+  <h1><u>Homepage</u></h1>
+</div>
+
+<div class="left-welcome-message">
+  <p>If you would like a better<br>experience and become<br>apart of your new favorite<br>blog site, join up, become<br>a member and upload your<br>own knowledge.</p>
+</div>
+<div class="middle-welcome-message">
+  <pr>Don’t want to join<br>up? Just enjoy the<br>blogs.</pr>
+</div>
+
+</body>
+</html>
